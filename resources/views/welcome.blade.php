9c5d0feff4b16935fe97150f1f16d5e974a0e257
yaojinhui <yaojinhui1993@gmail.com>
2018-02-19 19:36:01 +0800

    Change frontend preset to tailwind css.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,59 +1,57 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <title>Learn Vue</title>
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-  <style>
-    .color-red {
-      color: red;
-    }
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-    .color-blue {
-      color: blue;
-    }
-
-    .is-loading {
-      color: red;
-    }
-  </style>
+    <!-- Styles -->
+    <link href="{{ asset('css/main.css') }}" rel="stylesheet">
 </head>
-<body>
-<div id="app">
-  <button :title="title">Hover over me</button>
+<body class="bg-brand-lightest font-sans font-normal">
+    <div class="flex flex-col">
+        @if(Route::has('login'))
+            <div class="absolute pin-t pin-r mt-4 mr-4">
+                @auth
+                    <a href="{{ url('/home') }}" class="no-underline hover:underline text-sm font-normal text-brand-dark uppercase">Home</a>
+                @else
+                    <a href="{{ route('login') }}" class="no-underline hover:underline text-sm font-normal text-brand-dark uppercase pr-6">Login</a>
+                    <a href="{{ route('register') }}" class="no-underline hover:underline text-sm font-normal text-brand-dark uppercase">Register</a>
+                @endauth
+            </div>
+        @endif
 
-  <h1 :class="className">Title</h1>
-
-  <button :class="{ 'is-loading': isLoading }" @click="toggleClass">Click me toggle</button>
-
-  <button :disabled="isDisabled" @click="toggleDisabled">Click me disabled</button>
-</div>
-
-
-<script src="https://cdn.bootcss.com/vue/2.5.13/vue.js"></script>
-<script>
-  let app = new Vue({
-    el: '#app',
-    data: {
-      title: 'Now title is being set through the JavaScript.',
-      className: 'color-red',
-      isLoading: true,
-      isDisabled: false,
-    },
-    methods: {
-       toggleClass () {
-         this.isLoading = ! this.isLoading;
-       },
-
-       toggleDisabled () {
-         this.isDisabled = ! this.isDisabled;
-       }
-    }
-
-  })
-</script>
+        <div class="min-h-screen flex items-center justify-center">
+            <div class="flex flex-col justify-around h-full">
+                <div>
+                    <h1 class="text-grey-darker text-center font-hairline tracking-wide text-7xl mb-6">
+                        {{ config('app.name', 'Laravel') }}
+                    </h1>
+                    <ul class="list-reset">
+                        <li class="inline pr-8">
+                            <a href="https://laravel.com/docs" class="no-underline hover:underline text-sm font-normal text-brand-dark uppercase" title="Documentation">Documentation</a>
+                        </li>
+                        <li class="inline pr-8">
+                            <a href="https://laracasts.com" class="no-underline hover:underline text-sm font-normal text-brand-dark uppercase" title="Laracasts">Laracasts</a>
+                        </li>
+                        <li class="inline pr-8">
+                            <a href="https://laravel-news.com" class="no-underline hover:underline text-sm font-normal text-brand-dark uppercase" title="News">News</a>
+                        </li>
+                        <li class="inline pr-8">
+                            <a href="https://forge.laravel.com" class="no-underline hover:underline text-sm font-normal text-brand-dark uppercase" title="Forge">Forge</a>
+                        </li>
+                        <li class="inline pr-8">
+                            <a href="https://github.com/laravel/laravel" class="no-underline hover:underline text-sm font-normal text-brand-dark uppercase" title="GitHub">GitHub</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
